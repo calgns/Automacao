fb6c2e54dcbd172f3441558a4675d4898ab38c9a
Matheus Henrique <matheus110903@gmail.com>
2021-08-27 12:22:12 -0300

    feito pause e alerte e comentarios

--- Auto.py
+++ Auto.py
@@ -1,3 +1,10 @@
 import pyautogui #mouse e teclado
 import time
 import pyperclip #ctrl+c e v com pyper.
+
+#pyautogui.. serve para qualquer tecla com a win e pode coloca o texto direto, é dahora!
+# PAUSE significa pausar e na frente você coloca o tempo que quiser 1 = segundo, 60 segundos = 1 minuto
+
+pyautogui.PAUSE = 1
+# ele da um ALERTA!!!
+pyautogui.alert('NÃO MEXE,EU ESTOU USANDO O PC DEPOIS VOCÊ VOLTA')

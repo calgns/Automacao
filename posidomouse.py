1e8a3d3f3da553acef07f8de99b1d27239a63e35
Matheus Henrique <matheus110903@gmail.com>
2021-08-27 13:10:05 -0300

    descobrindo a posição do mouse

--- posidomouse.py
+++ posidomouse.py
@@ -1,2 +1,7 @@
 import pyautogui
 import time
+time.sleep(5)
+# para ver a posição do mouse  coloque acima do que vais clicar.
+print(pyautogui.position())
+# e assim tera a posição do local onde o mouse estava
+print("ala vai")
